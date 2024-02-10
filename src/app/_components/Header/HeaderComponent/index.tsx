3bdf69be7a8165577b743ea4e72172112e3e8d47
Yann Kamche <yannkamche@gmail.com>
2024-02-10 21:59:21 +0100

    Completed the Header, Navbar and footer, working on the Hero section

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -1,38 +1,50 @@
-"use client"
+'use client'
 
-import React from 'react'
-import Image from 'next/image'
+import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
+import { Header } from '../../../../payload/payload-types';
+import { Gutter } from '../../Gutter';
+import Link from 'next/link';
+import { HeaderNav } from '../Nav';
+import { noHeaderFooterUrls } from '../../../constants';
+import { usePathname } from 'next/navigation';
+import classes from './index.module.scss';
 
-import { Header } from '../../../../payload/payload-types'
-import { Gutter } from '../../Gutter'
-import Link from 'next/link'
+const HeaderComponent: React.FC<{ header: Header }> = ({ header }) => {
+    const pathname = usePathname();
+    const [isSticky, setIsSticky] = useState(false);
 
-import { HeaderNav } from '../Nav'
-import { noHeaderFooterUrls } from '../../../constants'
-import { usePathname } from 'next/navigation'
-// import MobileNav from '../MobileNav'
+    useEffect(() => {
+        const handleScroll = () => {
+            const navbarHeight = document.querySelector('.header')?.offsetHeight || 0;
+            if (window.pageYOffset > navbarHeight) {
+                setIsSticky(true);
+            } else {
+                setIsSticky(false);
+            }
+        };
 
-import classes from './index.module.scss'
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
 
-
-
-const HeaderComponent = ({ header}: {header: Header}) => {
-
-    const pathname = usePathname()
-  return (
-    <nav className={[classes.header, noHeaderFooterUrls.includes(pathname) && classes.hide]
+    const headerClasses = [classes.header, isSticky && classes.sticky, noHeaderFooterUrls.includes(pathname) && classes.hide]
         .filter(Boolean)
-        .join(' ')}>
-      <Gutter className={classes.wrap}>
-        <Link href="/">
-            <Image src="/logo-black.svg" alt="logo" width={170} height={50}/>
-        </Link>
+        .join(' ');
 
-        <HeaderNav header={header}/>
-        {/* <MobileNav header={header} /> */}
-      </Gutter>
-    </nav>
-  )
-}
+    return (
+        <nav className={headerClasses}>
+            <Gutter className={classes.wrap}>
+                <Link href="/">
+                    <Image src="/logo-black.svg" alt="logo" width={170} height={50} />
+                </Link>
+                <HeaderNav header={header} />
+                {/* <MobileNav header={header} /> */}
+            </Gutter>
+        </nav>
+    );
+};
 
-export default HeaderComponent
+export default HeaderComponent;
